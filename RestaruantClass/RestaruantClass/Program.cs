0da69319ef2ae773e8e2bbe953afc1f9d7cb139e
mrbuzb <mrbpubg14@gmail.com>
2024-11-19 07:52:20 -0700

    RestaruantClass

---
+++ RestaruantClass/RestaruantClass/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace RestaruantClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Restaruant firstRestaruant = new Restaruant()
+            {
+                RestaruantName = "AlBurj",
+                OpenedYear = 2022,
+                TypeOfRestaruant = "Italian",
+                MainMeal = "Kebab",
+                Adress = "Dubai",
+                Raiting = 10,
+                OpeningTime = "08:00",
+                ClosingTime = "21:00",
+                Dilevry = true,
+                Capisity = 900
+            };
+            Restaruant secondRestaruant = new Restaruant()
+            {
+                RestaruantName = "Bella Italian",
+                OpenedYear = 1234,
+                TypeOfRestaruant = "Italian",
+                MainMeal = "NonKebab",
+                Adress = "Uzbekistan",
+                Raiting = 2.5,
+                OpeningTime = "08:00",
+                ClosingTime = "21:00",
+                Dilevry = false,
+                Capisity = 3
+            };
+            Restaruant thirdRestaruant = new Restaruant()
+            {
+                RestaruantName = "Bella",
+                OpenedYear = 1200,
+                TypeOfRestaruant = "Brazilian",
+                MainMeal = "Soup",
+                Adress = "Paris",
+                Raiting = 5.3,
+                OpeningTime = "08:00",
+                ClosingTime = "21:00",
+                Dilevry = false,
+                Capisity = int.MaxValue
+            };
+
+            thirdRestaruant.Display();
+        }
+    }
+}
---
+++ RestaruantClass/RestaruantClass/Restaruant.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaruantClass;
+
+internal class Restaruant
+{
+    public string  RestaruantName { get; set; }
+    public int  OpenedYear { get; set; }
+    public string  TypeOfRestaruant { get; set; }
+    public string  MainMeal { get; set; }
+    public string  Adress { get; set; }
+    public double  Raiting { get; set; }
+    public string  OpeningTime { get; set; }
+    public string  ClosingTime { get; set; }
+    public bool  Dilevry { get; set; }
+    public int  Capisity { get; set; }
+
+
+
+    public void Display()
+    {
+        Console.WriteLine($"Name:{RestaruantName}");
+        Console.WriteLine($"OpenedYear:{OpenedYear}");
+        Console.WriteLine($"Type:{TypeOfRestaruant}");
+        Console.WriteLine($"MainMeal:{MainMeal}");
+        Console.WriteLine($"Adress:{Adress}");
+        Console.WriteLine($"Raiting:{Raiting}");
+        Console.WriteLine($"OpeningTime:{OpeningTime}");
+        Console.WriteLine($"ClosingTime:{ClosingTime}");
+        Console.WriteLine($"Dilevry:{Dilevry}");
+        Console.WriteLine($"Capisity:{Capisity}");
+    }
+}
