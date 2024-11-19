35e933f4e0f10728fb92c6ed2ab4ec328123d655
mrbuzb <mrbpubg14@gmail.com>
2024-11-19 07:21:47 -0700

    AnimalClass

---
+++ AnimalClass/AnimalClass/Animal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalClass;
+
+public class Animal
+{
+    public string AnimalName { get; set; }
+
+    public int AnimalAge { get; set; }
+
+    public string AnimalColor { get; set; }
+
+    public string TypeOfAnimal { get; set; }
+
+    public int AnimalHeight { get; set; }
+
+    public int AnimalWeight { get; set; }
+
+    public string BirthCountry { get; set; }
+
+    public string AnimalHabitat { get; set; }
+
+    public string AnimalDiet { get; set; }
+
+    public string AnimalGender { get; set; }
+
+    public void DisplayInformations()
+    {
+        var informations = $"Name:{AnimalName}\nAge:{AnimalAge}\nColor:{AnimalColor}\n" +
+            $"Type:{TypeOfAnimal}\nHeight:{AnimalHeight}\nWeight:{AnimalWeight}\nCountry:{BirthCountry}\nHabitat:{AnimalHabitat}\n" +
+            $"Diet:{AnimalDiet}\nGender:{AnimalGender}\n ";
+        Console.WriteLine(informations);
+    }
+}
---
+++ AnimalClass/AnimalClass/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace AnimalClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var animals=new List<Animal>();
+            Animal firstAnimal = new Animal()
+            {
+                BirthCountry ="Australie",
+                AnimalName = "Tiger",
+                TypeOfAnimal = "Mammal",
+                AnimalColor = "Brown",
+                AnimalAge = 2,
+                AnimalGender = "Male",
+                AnimalHeight = 1,
+                AnimalWeight = 50,
+                AnimalHabitat="Wild",
+                AnimalDiet = "Meat"
+            };
+
+            Animal secondAnimal = new Animal()
+            {
+                BirthCountry ="Australie",
+                AnimalName = "Lion",
+                TypeOfAnimal = "Mammal",
+                AnimalColor = "Brown",
+                AnimalAge = 5,
+                AnimalGender = "Male",
+                AnimalHeight = 1,
+                AnimalWeight = 80,
+                AnimalHabitat="Wild",
+                AnimalDiet = "Meat"
+            };
+            Animal thirdAnimal = new Animal()
+            {
+                BirthCountry ="Jungle",
+                AnimalName = "Bear",
+                TypeOfAnimal = "Mammal",
+                AnimalColor = "Brown",
+                AnimalAge = 3,
+                AnimalGender = "Female",
+                AnimalHeight = 3,
+                AnimalWeight = 100,
+                AnimalHabitat="Wild",
+                AnimalDiet = "Meat"
+            };
+
+            firstAnimal.DisplayInformations();
+            secondAnimal.DisplayInformations();
+            thirdAnimal.DisplayInformations();
+
+        }
+    }
+}
