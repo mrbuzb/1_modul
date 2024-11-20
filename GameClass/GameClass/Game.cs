e27f8a3b39280659103aee99cc218447fcd0b159
mrbuzb <mrbpubg14@gmail.com>
2024-11-20 04:57:38 -0700

    GameClass

---
+++ GameClass/GameClass/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClass
+{
+    internal class Game
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Status { get; set; }
+        public string Genre { get; set; }
+        public string PublishedDate { get; set; }
+        public double Price { get; set; }
+        public string language { get; set; }
+        public string PlayersCount { get; set; }
+        public double Raiting { get; set; }
+        public int Downloads { get; set; }
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name:{Name}");
+            Console.WriteLine($"Author:{Author}");
+            Console.WriteLine($"Status:{Status}");
+            Console.WriteLine($"Genre:{Genre}");
+            Console.WriteLine($"PublishedDate:{PublishedDate}");
+            Console.WriteLine($"Price:{Price}");
+            Console.WriteLine($"language:{language}");
+            Console.WriteLine($"PlayersCount:{PlayersCount}");
+            Console.WriteLine($"Raiting:{Raiting}");
+            Console.WriteLine($"Downloads:{Downloads}");
+        }
+    }
+}
---
+++ GameClass/GameClass/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace GameClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game firstGame = new Game()
+            {
+                Name = "SubwaySurfers",
+                Author = "I Dont Know",
+                Status = "Old",
+                Genre = "Run",
+                PublishedDate = "01.01.2010",
+                Price = 0,
+                language = "English",
+                PlayersCount = "1 bilion",
+                Raiting = 4.4,
+                Downloads = 100000000
+            };
+            Game secondGame = new Game()
+            {
+                Name = "KnifeHit",
+                Author = "I Dont Know",
+                Status = "Old",
+                Genre = "sniper",
+                PublishedDate = "01.01.2010",
+                Price = 0,
+                language = "English",
+                PlayersCount = "100 million",
+                Raiting = 4.3,
+                Downloads = 100000000
+            };
+            Game thirdGame = new Game()
+            {
+                Name = "WalkMaster",
+                Author = "I Dont Know",
+                Status = "New",
+                Genre = "Walk",
+                PublishedDate = "01.01.2020",
+                Price = 0,
+                language = "English",
+                PlayersCount = "10 million",
+                Raiting = 4.2,
+                Downloads = 10000000
+            };
+        }
+    }
+}
