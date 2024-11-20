82aaea07057bcd9b5b98de67a004e92885e1978d
mrbuzb <mrbpubg14@gmail.com>
2024-11-19 22:38:52 -0700

    HomeClass

---
+++ HomeClass/HomeClass/Home.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaruantClass;
+
+internal class Home
+{
+    public int Number { get; set; }
+    public int Year { get; set; }
+    public string Status { get; set; }
+    public int CoutnOfFloors { get; set; }
+    public string Adress { get; set; }
+    public double Raiting { get; set; }
+    public string OwnersName { get; set; }
+    public double Price { get; set; }
+    public int Capicity { get; set; }
+    public int CountOfRooms { get; set; }
+
+
+
+    public void Display()
+    {
+        Console.WriteLine($"Number:{Number}");
+        Console.WriteLine($"Year:{Year}");
+        Console.WriteLine($"Status:{Status}");
+        Console.WriteLine($"CoutnOfFloors:{CoutnOfFloors}");
+        Console.WriteLine($"Adress:{Adress}");
+        Console.WriteLine($"Raiting:{Raiting}");
+        Console.WriteLine($"OwnersName:{OwnersName}");
+        Console.WriteLine($"Price:{Price}");
+        Console.WriteLine($"Capicity:{Capicity}");
+        Console.WriteLine($"CountOfRooms:{CountOfRooms}");
+        Console.WriteLine("\n");
+    }
+}
---
+++ HomeClass/HomeClass/Program.cs
@@ -0,0 +1,56 @@
+﻿using RestaruantClass;
+
+namespace HomeClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Home firstHome = new Home()
+            {
+                Number = 15,
+                Year = 2004,
+                Status = "New",
+                CoutnOfFloors = 2,
+                Adress = "Garden Street 123",
+                Raiting = 5,
+                OwnersName = "Billy James",
+                Price = 999000,
+                Capicity = 100,
+                CountOfRooms = 20
+            };
+            
+            Home secondHome = new Home()
+            {
+                Number = 11,
+                Year = 2024,
+                Status = "New",
+                CoutnOfFloors = 6,
+                Adress = "Garden Street 101",
+                Raiting = 5,
+                OwnersName = "James Brain",
+                Price = 1000000,
+                Capicity = 500,
+                CountOfRooms = 60
+            };
+            Home thirdHome = new Home()
+            {
+                Number = 5,
+                Year = 2021,
+                Status = "Old",
+                CoutnOfFloors = 1,
+                Adress = "Garden Street 56",
+                Raiting = 3.5,
+                OwnersName = "James Kartes",
+                Price = 10000,
+                Capicity = 30,
+                CountOfRooms = 10
+            };
+
+            firstHome.Display();
+            secondHome.Display();
+            thirdHome.Display();
+
+        }
+    }
+}
