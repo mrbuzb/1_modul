ea1a48f0da3455ff131a1e7b981dc3010a873e51
mrbuzb <mrbpubg14@gmail.com>
2024-11-20 03:40:37 -0700

    SchoolClass

---
+++ SchoolClass/SchoolClass/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace SchoolClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            School firstSchool = new School()
+            {
+                OpenedYear = 2009,
+                Status = "New",
+                Capicity = 5000,
+                Raiting = 3,
+                FloorsCount = 6,
+                RoomsCount = 100,
+                Studentscount = 3500,
+                IsPersonal = false,
+                Specialisty = "Geography",
+                Number = 9
+            };
+            School secondSchool = new School()
+            {
+                OpenedYear = 2020,
+                Status = "New",
+                Capicity = 2000,
+                Raiting = 4,
+                FloorsCount = 4,
+                RoomsCount = 100,
+                Studentscount = 1500,
+                IsPersonal = true,
+                Specialisty = "art",
+                Number = 1
+            };
+            School thirdSchool = new School()
+            {
+                OpenedYear = 1991,
+                Status = "Old",
+                Capicity = 3000,
+                Raiting = 1,
+                FloorsCount = 5,
+                RoomsCount = 100,
+                Studentscount = 1000,
+                IsPersonal = false,
+                Specialisty = "Education",
+                Number = 99
+            };
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ SchoolClass/SchoolClass/School.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolClass;
+
+internal class School
+{
+    public int OpenedYear { get; set; }
+    public string Status { get; set; }
+    public int Capicity { get; set; }
+    public int Raiting { get; set; }
+    public int FloorsCount { get; set; }
+    public int RoomsCount { get; set; }
+    public int Studentscount { get; set; }
+    public bool IsPersonal { get; set; }
+    public string Specialisty { get; set; }
+    public int Number { get; set; }
+
+
+
+    public void Display()
+    {
+        Console.WriteLine($"OpenedYear:{OpenedYear}");
+        Console.WriteLine($"Status:{Status}");
+        Console.WriteLine($"Capicity:{Capicity}");
+        Console.WriteLine($"Raiting:{Raiting}");
+        Console.WriteLine($"FloorsCount:{FloorsCount}");
+        Console.WriteLine($"RoomsCount:{RoomsCount}");
+        Console.WriteLine($"Studentscount:{Studentscount}");
+        Console.WriteLine($"IsPersonal:{IsPersonal}");
+        Console.WriteLine($"Specialisty:{Specialisty}");
+        Console.WriteLine($"Number:{Number}");
+        Console.WriteLine("\n");
+    }
+
+}
