e909b93a40ee5bd1d8a82f93bde93b41b25d09bc
mrbuzb <mrbpubg14@gmail.com>
2024-11-20 03:59:21 -0700

    MovieClass

---
+++ MovieClass/MovieClass/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieClass;
+
+internal class Movie
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public string Status { get; set; }
+    public string Genre { get; set; }
+    public string PublishedDate { get; set; }
+    public double Price { get; set; }
+    public string language { get; set; }
+    public string Quality { get; set; }
+    public int Raiting { get; set; }
+    public string Time { get; set; }
+
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name:{Name}");
+        Console.WriteLine($"Author:{Author}");
+        Console.WriteLine($"Status:{Status}");
+        Console.WriteLine($"Genre:{Genre}");
+        Console.WriteLine($"PublishedDate:{PublishedDate}");
+        Console.WriteLine($"Price:{Price}");
+        Console.WriteLine($"language:{language}");
+        Console.WriteLine($"Quality:{Quality}");
+        Console.WriteLine($"Raiting:{Raiting}");
+        Console.WriteLine($"Time:{Time}");
+    }
+
+}
---
+++ MovieClass/MovieClass/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace MovieClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Movie firstMovie = new Movie()
+            {
+                Name = "UyquchiKelin",
+                Author = "Axad Qayum",
+                Status = "New",
+                Genre = "Vital",
+                PublishedDate = "01.01.2024",
+                Price = 0,
+                language = "Uzbek",
+                Quality = "16K",
+                Raiting = 0,
+                Time = "1h,59m,59s"
+            };
+            Movie secondMovie = new Movie()
+            {
+                Name = "MenKimman",
+                Author = "Chin Chan xo",
+                Status = "Old",
+                Genre = "Funny",
+                PublishedDate = "01.01.2000",
+                Price = 0,
+                language = "Any",
+                Quality = "1080p",
+                Raiting = 1,
+                Time = "1h,29m,59s"
+            };
+            Movie thirdMovie = new Movie()
+            {
+                Name = "Avatar",
+                Author = "Nobody Knows",
+                Status = "Old",
+                Genre = "Fantastic",
+                PublishedDate = "01.01.2001",
+                Price = 0,
+                language = "English",
+                Quality = "1080p",
+                Raiting = 2,
+                Time = "1h,49m,59s"
+            };
+        }
+    }
+}
