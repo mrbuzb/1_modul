31a170722ad22609719751235e2557291a9ef792
mrbuzb <mrbpubg14@gmail.com>
2024-11-19 22:59:31 -0700

    PhoneClass

---
+++ PhoneClass_/PhoneClass_/Phone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaruantClass;
+
+internal class Phone
+{
+    public int PublishedYear { get; set; }
+    public string Status { get; set; }
+    public int PhoneMemory { get; set; }
+    public int Ram { get; set; }
+    public string Model { get; set; }
+    public string Name { get; set; }
+    public double Raiting { get; set; }
+    public string Price { get; set; }
+    public int CameraMP { get; set; }
+    public bool Document { get; set; }
+
+
+
+    public void Display()
+    {
+        Console.WriteLine($"PublishedYear:{PublishedYear}");
+        Console.WriteLine($"Status:{Status}");
+        Console.WriteLine($"PhoneMemory:{PhoneMemory}");
+        Console.WriteLine($"Ram:{Ram}");
+        Console.WriteLine($"Model:{Model}");
+        Console.WriteLine($"Name:{Name}");
+        Console.WriteLine($"Price:{Price}");
+        Console.WriteLine($"CameraMP:{CameraMP}");
+        Console.WriteLine($"Document:{Document}");
+        Console.WriteLine($"Raiting:{Raiting}");
+        Console.WriteLine("\n");
+    }
+}
---
+++ PhoneClass_/PhoneClass_/Program.cs
@@ -0,0 +1,55 @@
+﻿using RestaruantClass;
+
+namespace PhoneClass_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone firstPhone = new Phone()
+            {
+                PublishedYear = 2023,
+                Status = "New",
+                PhoneMemory = 128,
+                Ram = 4,
+                Model = "Xiaomi",
+                Name = "Note 11",
+                Raiting = 3.5,
+                Price = "200",
+                CameraMP = 48,
+                Document = true
+            };
+            
+            Phone secondPhone = new Phone()
+            {
+                PublishedYear = 2000,
+                Status = "Old",
+                PhoneMemory = 0,
+                Ram = 0,
+                Model = "Nokia",
+                Name = "1202",
+                Raiting = 5,
+                Price = "Infinity",
+                CameraMP = 0,
+                Document = false
+            };
+
+            Phone thirdPhone = new Phone()
+            {
+                PublishedYear = 2023,
+                Status = "New",
+                PhoneMemory = 256,
+                Ram = 8,
+                Model = "Xiaomi",
+                Name = "Note 13",
+                Raiting = 5,
+                Price = "300",
+                CameraMP = 200,
+                Document = true
+            };
+
+            secondPhone.Display();
+
+        }
+    }
+}
