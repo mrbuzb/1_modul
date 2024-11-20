343f4fad5c8fcb1d0bf93f48068a29be644e385a
mrbuzb <mrbpubg14@gmail.com>
2024-11-20 04:44:54 -0700

    ComputerClass

---
+++ ComputerClass/ComputerClass/Computer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerClass;
+
+internal class Computer
+{
+    public int PublishedYear { get; set; }
+    public string Status { get; set; }
+    public int ComputerMemory { get; set; }
+    public int Ram { get; set; }
+    public string Model { get; set; }
+    public string Name { get; set; }
+    public double Raiting { get; set; }
+    public string Price { get; set; }
+    public string VideoCard { get; set; }
+    public bool Document { get; set; }
+
+
+
+    public void Display()
+    {
+        Console.WriteLine($"PublishedYear:{PublishedYear}");
+        Console.WriteLine($"Status:{Status}");
+        Console.WriteLine($"ComputerMemory:{ComputerMemory}");
+        Console.WriteLine($"Ram:{Ram}");
+        Console.WriteLine($"Model:{Model}");
+        Console.WriteLine($"Name:{Name}");
+        Console.WriteLine($"Price:{Price}");
+        Console.WriteLine($"VideoCard:{VideoCard}");
+        Console.WriteLine($"Document:{Document}");
+        Console.WriteLine($"Raiting:{Raiting}");
+        Console.WriteLine("\n");
+    }
+}
---
+++ ComputerClass/ComputerClass/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Numerics;
+using System.Threading.Tasks.Sources;
+
+namespace ComputerClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer firstComputer = new Computer()
+            {
+                PublishedYear = 2023,
+                Status = "New",
+                ComputerMemory = 512,
+                Ram = 16,
+                Model = "Hp",
+                Name = "Victus",
+                Raiting = 4.5,
+                Price = "900",
+                VideoCard = "Core i5",
+                Document = true
+            };
+            Computer secondComputer = new Computer()
+            {
+                PublishedYear = 2015,
+                Status = "Old",
+                ComputerMemory = 1,
+                Ram = 4,
+                Model = "Lenovo",
+                Name = "tr 500",
+                Raiting = 1.5,
+                Price = "200",
+                VideoCard = "No",
+                Document = false
+            };
+            Computer thirdComputer = new Computer()
+            {
+                PublishedYear = 2000,
+                Status = "Old",
+                ComputerMemory = 4,
+                Ram = 2,
+                Model = "Lg",
+                Name = "lg 5",
+                Raiting = 1,
+                Price = "100",
+                VideoCard = "No",
+                Document = false
+            };
+        }
+    }
+}
