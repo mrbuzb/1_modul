8b0ca191bfd0d3b3ebb167d13b4766ce32056dda
mrbuzb <mrbpubg14@gmail.com>
2024-11-20 05:10:41 -0700

    PlanetClass

---
+++ PlanetClass/PlanetClass/Planet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetClass;
+
+internal class Planet
+{
+    public  string Name { get; set; }
+    public  int Population { get; set; }
+    public  string MainArea { get; set; }
+    public  string Creatures { get; set; }
+    public  int ProximitySun { get; set; }
+    public  int Length { get; set; }
+    public  int Degree { get; set; }
+
+    public void Display()
+    {
+        Console.WriteLine($"Name:{Name}");
+        Console.WriteLine($"Population:{Population}");
+        Console.WriteLine($"MainArea:{MainArea}");
+        Console.WriteLine($"Creatures:{Creatures}");
+        Console.WriteLine($"ProximitySun:{ProximitySun}");
+        Console.WriteLine($"Length:{Length}");
+        Console.WriteLine($"Degree:{Degree}");
+    }
+}
---
+++ PlanetClass/PlanetClass/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PlanetClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Planet firstPlanet = new Planet()
+            {
+                Name = "Mars",
+                Population = 0,
+                MainArea = "Qum",
+                Creatures = "Maybe Alians",
+                ProximitySun = 45445654,
+                Length = 65646546,
+                Degree = 55
+            };
+        }
+    }
+}
